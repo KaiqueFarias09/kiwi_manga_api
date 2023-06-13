42a8c18d9151ca607eb457dc32c264a6f0ed91d6
Kaique Farias <kaiquesouzafarias@outlook.com>
2023-06-13 10:46:59 -0300

    feat: :sparkles: add swagger and dto validation with helmet

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,28 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import helmet from 'helmet';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT || 3000);
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-    }),
-  );
+
+  app.use(helmet());
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  app.setGlobalPrefix('api/v1');
+  app.enableCors();
+
+  const config = new DocumentBuilder()
+    .setTitle('Kiwi API Documentation')
+    .setDescription(' A API that provides programmatic access to manga data')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
+  await app.listen(process.env.PORT || 3000, () => {
+    console.log(`Server is listening on port ${process.env.PORT || 3000}`);
+  });
 }
+
 bootstrap();
