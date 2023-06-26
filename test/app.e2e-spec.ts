c75d55a5fb7ba65056ab176f3973620ed3174e20
Kaique Farias <kaiquesouzafarias@outlook.com>
2023-06-26 19:33:01 -0300

    test: :white_check_mark: create basic e2e tests

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,24 +1,111 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
+import { Test } from '@nestjs/testing';
+import * as pactum from 'pactum';
+import { PostgresService } from 'src/frameworks/postgres-prisma/postgres-prisma.service';
+import { AppModule } from '../src/app.module';
 
-describe('AppController (e2e)', () => {
+describe('App e2e', () => {
   let app: INestApplication;
+  let postgresService: PostgresService;
 
-  beforeEach(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
-    app = moduleFixture.createNestApplication();
+    app = moduleRef.createNestApplication();
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,
+      }),
+    );
     await app.init();
+    await app.listen(3333);
+
+    postgresService = app.get(PostgresService);
+    await postgresService.cleanDb();
+    pactum.request.setBaseUrl('http://localhost:3333');
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  afterAll(() => {
+    app.close();
   });
+
+  describe('Health', () => {
+    describe('Get health', () => {
+      it('should return health', () => {
+        return pactum.spec().get('/health').expectStatus(200);
+      });
+    });
+  });
+
+  // describe('Auth', () => {
+  //   const dto: SignupDto = {
+  //     email: 'vlad@gmail.com',
+  //     password: '123',
+  //     nickname: 'vlad',
+  //   };
+  //   describe('Signup', () => {
+  //     it('should throw if email empty', () => {
+  //       return pactum
+  //         .spec()
+  //         .post('/auth/signup')
+  //         .withBody({
+  //           password: dto.password,
+  //         })
+  //         .expectStatus(400);
+  //     });
+  //     it('should throw if password empty', () => {
+  //       return pactum
+  //         .spec()
+  //         .post('/auth/signup')
+  //         .withBody({
+  //           email: dto.email,
+  //         })
+  //         .expectStatus(400);
+  //     });
+  //     it('should throw if no body provided', () => {
+  //       return pactum.spec().post('/auth/signup').expectStatus(400);
+  //     });
+  //     it('should signup', () => {
+  //       return pactum
+  //         .spec()
+  //         .post('/auth/signup')
+  //         .withBody(dto)
+  //         .expectStatus(201);
+  //     });
+  //   });
+
+  // describe('Signin', () => {
+  //   it('should throw if email empty', () => {
+  //     return pactum
+  //       .spec()
+  //       .post('/auth/signin')
+  //       .withBody({
+  //         password: dto.password,
+  //       })
+  //       .expectStatus(400);
+  //   });
+  //   it('should throw if password empty', () => {
+  //     return pactum
+  //       .spec()
+  //       .post('/auth/signin')
+  //       .withBody({
+  //         email: dto.email,
+  //       })
+  //       .expectStatus(400);
+  //   });
+  //   it('should throw if no body provided', () => {
+  //     return pactum.spec().post('/auth/signin').expectStatus(400);
+  //   });
+  //   it('should signin', () => {
+  //     return pactum
+  //       .spec()
+  //       .post('/auth/signin')
+  //       .withBody(dto)
+  //       .expectStatus(200)
+  //       .stores('userAt', 'access_token');
+  //   });
+  // });
+  // });
 });
