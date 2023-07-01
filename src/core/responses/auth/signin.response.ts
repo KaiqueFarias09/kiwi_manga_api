208288f2ff0320d3d72e62aeb2f6d7780a4473f0
Kaique Farias <kaiquesouzafarias@outlook.com>
2023-07-01 11:26:12 -0300

    docs: :memo: add default response body to auth endpoints

--- src/core/responses/auth/signin.response.ts
+++ src/core/responses/auth/signin.response.ts
@@ -1,8 +1,17 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { AccessTokenEntity } from '../../entities';
 import { DefaultHttpResponse } from '../common';
 
 export class SigninHttpResponse extends DefaultHttpResponse<AccessTokenEntity> {
+  @ApiProperty({ example: 'success' })
   status: string;
+
+  @ApiProperty({
+    example: {
+      accessToken:
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c',
+    },
+  })
   data: AccessTokenEntity;
 
   constructor(status: string, data: AccessTokenEntity) {
--- src/core/responses/auth/signup.response.ts
+++ src/core/responses/auth/signup.response.ts
@@ -1,14 +1,27 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { DefaultHttpResponse } from '../common';
 
+export class SignupResponse {
+  @ApiProperty({
+    example:
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c',
+  })
+  access_token: string;
+}
+
 export class SignupHttpResponse extends DefaultHttpResponse<SignupResponse> {
+  @ApiProperty({ example: 'success' })
   status: string;
+
+  @ApiProperty({
+    example: {
+      access_token:
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c',
+    },
+  })
   data: SignupResponse;
 
   constructor(status: string, data: SignupResponse) {
     super(status, data);
   }
-}
-
-export class SignupResponse {
-  access_token: string;
 }
