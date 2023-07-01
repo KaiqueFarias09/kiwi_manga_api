e138142046207013e472451dfc0fd4692333be2a
Kaique Farias <kaiquesouzafarias@outlook.com>
2023-07-01 11:25:19 -0300

    docs: :memo: add default response body to chapters endpoints

--- src/core/responses/chapters/get-chapter.response.ts
+++ src/core/responses/chapters/get-chapter.response.ts
@@ -1,14 +1,27 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { DefaultHttpResponse } from '../common';
 
+class GetChapterResponse {
+  @ApiProperty({
+    type: 'array',
+    items: { type: 'string' },
+    example: [
+      'https://example.com/page1.jpg',
+      'https://example.com/page2.jpg',
+      'https://example.com/page3.jpg',
+    ],
+  })
+  pages: string[];
+}
+
 export class GetChapterHttpResponse extends DefaultHttpResponse<GetChapterResponse> {
-  data: GetChapterResponse;
+  @ApiProperty({ example: 'success' })
   status: string;
+
+  @ApiProperty({ type: GetChapterResponse })
+  data: GetChapterResponse;
 
   constructor(status: string, data: GetChapterResponse) {
     super(status, data);
   }
-}
-
-class GetChapterResponse {
-  pages: string[];
 }
--- src/core/responses/chapters/get-chapters.response.ts
+++ src/core/responses/chapters/get-chapters.response.ts
@@ -1,15 +1,43 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Chapter } from '../../entities';
 import { DefaultHttpResponse } from '../common';
 
+class GetChaptersResponse {
+  @ApiProperty({
+    type: Chapter,
+    isArray: true,
+    example: [
+      {
+        name: 'Chapter 1',
+        url: 'https://example.com/chapter1',
+        releasedAt: '2023-06-30T12:00:00Z',
+        pages: [
+          'https://example.com/page1.jpg',
+          'https://example.com/page2.jpg',
+        ],
+      },
+      {
+        name: 'Chapter 2',
+        url: 'https://example.com/chapter2',
+        releasedAt: '2023-06-31T12:00:00Z',
+        pages: [
+          'https://example.com/page3.jpg',
+          'https://example.com/page4.jpg',
+        ],
+      },
+    ],
+  })
+  chapters: Chapter[];
+}
+
 export class GetChaptersHttpResponse extends DefaultHttpResponse<GetChaptersResponse> {
-  data: GetChaptersResponse;
+  @ApiProperty({ example: 'success' })
   status: string;
+
+  @ApiProperty({ type: GetChaptersResponse })
+  data: GetChaptersResponse;
 
   constructor(status: string, data: GetChaptersResponse) {
     super(status, data);
   }
-}
-
-class GetChaptersResponse {
-  chapters: Chapter[];
 }
