dd28466835af685852a020669654b9bfcc8a304b
Kaique Farias <kaiquesouzafarias@outlook.com>
2023-07-08 19:21:44 -0300

    refactor: :recycle: remove ApiSecurity from AppModule and remove passwordVerifier middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -23,12 +23,10 @@ import {
 
 import { CacheModule } from '@nestjs/cache-manager';
 import { ConfigModule } from '@nestjs/config';
-import { ApiSecurity } from '@nestjs/swagger';
 import { UsersController } from './controllers/users.controller';
 import { MongoPrismaModule } from './frameworks/mongo-prisma/mongo-prisma.module';
 import { PostgresPrismaModule } from './frameworks/postgres-prisma/postgres-prisma.module';
 import { ScraperServiceModule } from './frameworks/scraper/scraper-service.module';
-import { PasswordVerifierMiddleware } from './middlewares/password-verifier.middleware';
 import { RequestLoggerMiddleware } from './middlewares/request-logger.middleware';
 import { UsersUseCaseModule } from './use-cases/users';
 
@@ -66,10 +64,9 @@ import { UsersUseCaseModule } from './use-cases/users';
     ScraperServiceModule,
   ],
 })
-@ApiSecurity('Authorization')
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
     consumer.apply(RequestLoggerMiddleware).forRoutes('*');
-    consumer.apply(PasswordVerifierMiddleware).forRoutes('users');
+    // consumer.apply(PasswordVerifierMiddleware).forRoutes('users');
   }
 }
